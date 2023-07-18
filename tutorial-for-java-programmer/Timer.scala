46d4c92eb5801312a3937328448a8efa08fcd026
Tobetch <takutobe@gmail.com>
2023-07-18 18:18:57 +0900

    everything is an object

---
+++ tutorial-for-java-programmer/Timer.scala
@@ -0,0 +1,13 @@
+object Timer {
+  // "Unit" equals "void" in Java
+  def oncePerSecond(callback: () => Unit): Unit = {
+    while (true) { callback(); Thread sleep 1000 }
+  }
+  def timeFlies(): Unit = {
+    // this "println" is not in "System.out".
+    println("time flies like an arrow...")
+  }
+  def main(args: Array[String]): Unit = {
+    oncePerSecond(timeFlies)
+  }
+}
