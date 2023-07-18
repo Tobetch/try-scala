8e1ef1371e6ad9af647669915233fa605e708591
Tobetch <takutobe@gmail.com>
2023-07-18 18:34:23 +0900

    Class with parameter

---
+++ tutorial-for-java-programmer/Complex.scala
@@ -0,0 +1,6 @@
+// classw with parameter, use like "new Complex(1.5, 2.3)"
+class Complex(real: Double, imaginary: Double) {
+  // methods don't have return value, but compiler infers as "Double" type
+  def re() = real
+  def im() = imaginary
+}
