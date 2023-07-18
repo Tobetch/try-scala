8d108fe22ad06c7e9afba39afbf2b2368fcb345e
Tobetch <takutobe@gmail.com>
2023-07-18 18:21:43 +0900

    anonymous function

--- tutorial-for-java-programmer/Timer.scala
+++ tutorial-for-java-programmer/Timer.scala
@@ -3,11 +3,15 @@ object Timer {
   def oncePerSecond(callback: () => Unit): Unit = {
     while (true) { callback(); Thread sleep 1000 }
   }
-  def timeFlies(): Unit = {
-    // this "println" is not in "System.out".
-    println("time flies like an arrow...")
-  }
+
+//   def timeFlies(): Unit = {
+//     // this "println" is not in "System.out".
+//     println("time flies like an arrow...")
+//   }
+
   def main(args: Array[String]): Unit = {
-    oncePerSecond(timeFlies)
+    oncePerSecond(() =>
+      println("time flies like an arrow...")
+    ) // Anonymous function
   }
 }
