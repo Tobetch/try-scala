a3dbd68184f4794adcf47f568712f64528b30cc5
Tobetch <takutobe@gmail.com>
2023-07-19 15:02:38 +0900

    Inheritance and overriding

--- tutorial-for-java-programmer/ComplexNumbers.scala
+++ tutorial-for-java-programmer/ComplexNumbers.scala
@@ -3,5 +3,6 @@ object ComplexNumbers {
     val c = new Complex(1.2, 3.4)
     // println("imaginary part: " + c.im())
     println("imaginary part: " + c.im) // abbreviate "()"
+    println("Overridden toString(): " + c.toString) // ex. override
   }
 }
